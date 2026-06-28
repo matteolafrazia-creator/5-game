a1648a8db5ec417929b6f6a3826e2b96f6055e7a
Matteo <matteo.lafrazia@gmail.com>
2026-06-28 22:38:51 +0200

    fix render http websocket

--- server.js
+++ server.js
@@ -1,20 +1,11 @@
-const http = require("http");
-const WebSocket = require("ws");
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 10000;
-
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Server 5 online");
+app.get('/', (req, res) => {
+	res.send('Hello World!');
 });
 
-const wss = new WebSocket.Server({ server });
-
-wss.on("connection", (ws) => {
-  console.log("Player connesso");
-  ws.send("OK - Server 5 attivo");
-});
-
-server.listen(PORT, () => {
-  console.log("Server 5 avviato su porta " + PORT);
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
 });
